967625aa5598e9ae2af64ea7ee82b8f5577c1198
Rian.Goossens <rian.goossens@ugent.be>
2015-11-05 00:17:11 +0100

    Genoeg geshizzled voor vandaag

--- feature_validation.py
+++ feature_validation.py
@@ -6,21 +6,21 @@ Created on Wed Nov  4 15:50:00 2015
 """
 import sklearn.cross_validation as cv
 import numpy
-from sklearn import linear_model
+from sklearn import lda
 from sklearn import cross_validation
 import score_calculation
 
-def validate_feature_linear(features, classes, n_folds = 5, print_folds = True):
+def validate_feature_linear(features, classes, n_folds = 5, print_folds = True, print_absolute = True, print_logloss = True):
     amount = len(features)
     kfold = cv.KFold(amount, n_folds = n_folds, shuffle = True)
-    model = linear_model.LogisticRegression(class_weight='auto')
+    model = lda.LDA()
     score = cross_validation.cross_val_score(model, features, classes, cv=kfold)
-    print("absolute scores")
+    if print_absolute: print("absolute scores")
     if print_folds: print("\tfolds:", score)
-    print("\tmean:", score.mean(), "std:", numpy.std(score))
+    if print_absolute: print("\tmean:", score.mean(), "std:", numpy.std(score))
 
-    model = linear_model.LogisticRegression(class_weight='auto')
+    model = lda.LDA()
     scores = score_calculation.loglossKFold(features, classes, model, kfold, given_kfold = True)
-    print("logloss scores")
+    if print_logloss: print("logloss scores")
     if print_folds: print("\tfolds",scores)
-    print("\tmean:", numpy.mean(scores), "std:", numpy.std(scores))
+    if print_logloss: print("\tmean:", numpy.mean(scores), "std:", numpy.std(scores))
