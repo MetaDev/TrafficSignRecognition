eda44ed977e83e135c472094288591e57326706e
pieterdc1 <pieter.decremer@ugent.be>
2015-11-05 15:39:27 +0100

    final report file finished

--- report_1_feature_extraction_data.py
+++ report_1_feature_extraction_data.py
@@ -11,15 +11,19 @@ import image_operations as operations
 import data_loading as loader
 import util
 import numpy
+from skimage import color
 
 #preloading
 print("loading data...")
+#images, classes = loader.loadUniqueTrainingAndClasses()
 images, classes = loader.loadTrainingAndClasses()
 amount = len(images)
 print("resizing...")
 resized = util.loading_map(lambda x : operations.cropAndResize(x, 0, 50), images)
 print("normalizing...")
 normalized = util.loading_map(operations.normalizeImage, resized)
+print("grayscaling...")
+grayscale = util.loading_map(color.rgb2gray, resized)
 #print("reducing color space...")
 #reduced = util.loading_map(operations.reduceColorSpace, resized)
 
@@ -28,8 +32,10 @@ normalized = util.loading_map(operations.normalizeImage, resized)
 #color_features                  = util.loading_map(extraction.color_features, resized)
 #print("normalized color features...")
 #normalized_color_features       = util.loading_map(extraction.color_features, normalized)
-print("split normalized color features...")
+print("mean channels features...")
 split_color_features            = util.loading_map(lambda x: extraction.split_image_features(extraction.color_features, 3, x), resized)
+print("moment channel features...")
+moment_channel_features            = util.loading_map(lambda x: extraction.split_image_features(lambda y: extraction.color_features(y,True,True,True,True), 3, x), resized)
 #print("split normalized color features...")
 #split_normalized_color_features = util.loading_map(lambda x: extraction.split_image_features(extraction.color_features, 3, x), normalized)
 #print("angle features")
@@ -38,16 +44,59 @@ print("weighted angle features")
 weighted_angle_features         = util.loading_map(lambda x : extraction.weightedAngleFeatures(x, 7), resized)
 print("Perceived brightness features")
 brightness_features             = util.loading_map(extraction.calculateDarktoBrightRatio, resized)
+print("High Pass Frequency features")
+frequency_features              = util.loading_map(lambda x : extraction.frequencyFeatures(x,selectedclasses=[22,23])[1::8], grayscale)
 
-combined_features = split_color_features.copy()
+
+combined_features1 = []
 for i in range(amount):
-    combined_features[i] = numpy.append(combined_features[i], weighted_angle_features[i])
+    combined_feature = numpy.array([])
+    combined_feature = numpy.append(combined_feature, split_color_features[i])
+    combined_feature = numpy.append(combined_feature, weighted_angle_features[i])
+    combined_features1.append(combined_feature)
 
-n_folds = 8
+combined_features2 = []
+for i in range(amount):
+    combined_feature = numpy.array([])
+    combined_feature = numpy.append(combined_feature, split_color_features[i])
+    combined_feature = numpy.append(combined_feature, brightness_features[i])
+    combined_features2.append(combined_feature)
+    
+combined_features3 = []
+for i in range(amount):
+    combined_feature = numpy.array([])
+    combined_feature = numpy.append(combined_feature, split_color_features[i])
+    combined_feature = numpy.append(combined_feature, brightness_features[i])
+    combined_feature = numpy.append(combined_feature, frequency_features[i])
+    combined_features3.append(combined_feature)
+    
+combined_features4 = []
+for i in range(amount):
+    combined_feature = numpy.array([])
+    combined_feature = numpy.append(combined_feature, split_color_features[i])
+    combined_feature = numpy.append(combined_feature, weighted_angle_features[i])
+    combined_feature = numpy.append(combined_feature, brightness_features[i])
+    combined_features4.append(combined_feature)
+    
+combined_features5 = []
+for i in range(amount):
+    combined_feature = numpy.array([])
+    combined_feature = numpy.append(combined_feature, split_color_features[i])
+    combined_feature = numpy.append(combined_feature, weighted_angle_features[i])
+    combined_feature = numpy.append(combined_feature, brightness_features[i])
+    combined_feature = numpy.append(combined_feature, frequency_features[i])
+    combined_features5.append(combined_feature)
+    
+    
+
+n_folds = 5
 
 #model evaluation
-print("Evaluating split color features")
+print("Evaluating mean channels features")
 validation.validate_feature_linear(split_color_features,    classes, n_folds, False, False, True)
+print("---")
+print("Evaluating moment channels features")
+validation.validate_feature_linear(moment_channel_features,    classes, n_folds, False, False, True)
 print("---")
 print("Evaluating weighted angle features")
 validation.validate_feature_linear(weighted_angle_features, classes, n_folds, False, False, True)
@@ -55,8 +104,19 @@ print("---")
 print("Evaluating perceived brightness features")
 validation.validate_feature_linear(brightness_features,     classes, n_folds, False, False, True)
 print("---")
-print("Evaluating combined features")
-validation.validate_feature_linear(combined_features,       classes, n_folds, False, False, True)
+print("Evaluating High pass frequency features")
+validation.validate_feature_linear(frequency_features,      classes, n_folds, False, False, True)
+print("---")
+print("Evaluating combined features: mean channels & weigthed angle")
+validation.validate_feature_linear(combined_features1,       classes, n_folds, False, False, True)
+print("Evaluating combined features: mean channels & perceived brightness")
+validation.validate_feature_linear(combined_features2,       classes, n_folds, False, False, True)
+print("Evaluating combined features: mean channels & perceived brightness & frequency")
+validation.validate_feature_linear(combined_features3,       classes, n_folds, False, False, True)
+print("Evaluating combined features: mean channels & perceived brightness & weighted angle")
+validation.validate_feature_linear(combined_features4,       classes, n_folds, False, False, True)
+print("Evaluating combined features: mean channels & perceived brightness & weighted angle & frequency")
+validation.validate_feature_linear(combined_features5,       classes, n_folds, False, False, True)
 
 #lower performing features
 #print("Evaluating color features")
