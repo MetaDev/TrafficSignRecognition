eef9be545bbbb8cd89e165d53d95fb656299fcc7
pieterdc1 <pieter.decremer@ugent.be>
2015-12-03 10:44:02 +0100

    still something wrong with dimensions for some reason

--- best_models_featureless_covnet.py
+++ best_models_featureless_covnet.py
@@ -41,7 +41,7 @@ resized = util.loading_map(lambda x : operations.cropAndResize(x, 0, size), imag
 print("reshaping to array...")
 size1_F = len(resized[0])
 size2_F = len(resized[0][0])
-print("size:",size1_F,",",size2_F)
+#♥print("size:",size1_F,",",size2_F)
 array = numpy.array(resized[0])
 reshaped = numpy.reshape(array,(1,size1_F*size2_F*3))
 reshaped = [reshaped]
@@ -49,13 +49,17 @@ for i in range(1,amount):
     size1 = len(resized[i])
     size2 = len(resized[i][0])
     if(size1*size2 != size1_F*size2_F):print("size:",size1,",",size2)
+    if(i%100 == 0):print(i,"/",amount)
     array = numpy.array(resized[i])
     a = numpy.reshape(array,(1,size1*size2*3))
     reshaped = numpy.concatenate((reshaped,[a]),0)
     
+print("dimensions reshaped:",len(reshaped),",",len(reshaped[0]),",",len(reshaped[0][0]))
+    
 
 #model = svm.SVC(kernel='linear', probability = True)
 from sklearn import random_projection
+print("model")
 model = Pipeline([
     #("standard scaler", StandardScaler()),
     #MinMaxScaler((-1,1)),
