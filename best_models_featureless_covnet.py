7321e39026228f85d892011d4443efe14f2a3058
pieterdc1 <pieter.decremer@ugent.be>
2015-12-03 09:36:58 +0100

    raw pixels as input for MLP

---
+++ best_models_featureless_covnet.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  4 16:02:41 2015
+
+@author: Rian
+"""
+
+import feature_validation as validation
+import feature_extraction as extraction
+import image_operations as operations
+import data_loading as loader
+import util
+import numpy
+import csv_output
+from sklearn import lda, svm
+from skimage import feature, color, exposure
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neural_network import MLPClassifier
+
+
+def calcHOG(image,orient=8,nr_of_cells_per_image=6,nr_of_cells_per_block=2, normalise = False):
+   height = len(image)
+   width = len(image[0]) 
+   ppc=(height/nr_of_cells_per_image,width/nr_of_cells_per_image)
+   cpb=(nr_of_cells_per_block, nr_of_cells_per_block)
+   fd = feature.hog(image, orientations=orient, pixels_per_cell=ppc,cells_per_block=cpb, normalise = normalise)
+   return numpy.array(fd).flatten()
+   
+#preloading
+print("loading data...")
+images, labels, classes = loader.loadTrainingImagesPoleNumbersAndClasses()
+amount = len(images)
+
+n_folds = 5
+
+#model = svm.SVC(kernel='linear', probability = True)
+from sklearn import random_projection
+model = Pipeline([
+    #("standard scaler", StandardScaler()),
+    MinMaxScaler((-1,1)),
+    #("principal component analysis", PCA(192)), #<- appears to reduce efficiency
+    #("lda projection", lda.LDA(n_components = 80)),
+    ("gaussian random projection", random_projection.GaussianRandomProjection(n_components = 150)),
+    #("sparse random projection", random_projection.SparseRandomProjection(n_components = 350)),
+    ("Multi-layer Perceptron", MLPClassifier(algorithm='adam', hidden_layer_sizes=(5,3,3,3,3,3,64,64,10), random_state=1,learning_rate='invscaling'))
+    #("svm", svm.SVC(kernel = "sigmoid", C = 1000, gamma = 0.0001, probability = True))
+    ])
+    
+print("Evaluating featureless covnet")
+validation.validate_feature(images, labels, classes, model, n_folds, False, False, True, True)
+print('\a')
