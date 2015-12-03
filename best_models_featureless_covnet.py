23e57fe3dfae36516777068536539e734ff00c26
pieterdc1 <pieter.decremer@ugent.be>
2015-12-03 14:15:57 +0100

    luv hsv

--- best_models_featureless_covnet.py
+++ best_models_featureless_covnet.py
@@ -22,13 +22,25 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.neural_network import MLPClassifier
 
 
-def calcHOG(image,orient=8,nr_of_cells_per_image=6,nr_of_cells_per_block=2, normalise = False):
-   height = len(image)
-   width = len(image[0]) 
-   ppc=(height/nr_of_cells_per_image,width/nr_of_cells_per_image)
-   cpb=(nr_of_cells_per_block, nr_of_cells_per_block)
-   fd = feature.hog(image, orientations=orient, pixels_per_cell=ppc,cells_per_block=cpb, normalise = normalise)
-   return numpy.array(fd).flatten()
+def flatten(resized):
+    size1_F = len(resized[0])
+    size2_F = len(resized[0][0])
+    size3_F = len(resized[0][0][0])
+    #♥print("size:",size1_F,",",size2_F)
+    array = numpy.array(resized[0])
+    reshaped = numpy.reshape(array,(size1_F*size2_F*size3_F))
+    reshaped = [reshaped]
+    for i in range(1,amount):
+        size1 = len(resized[i])
+        size2 = len(resized[i][0])
+        size3 = len(resized[i][0][0])
+        if(size1*size2*size3 != size1_F*size2_F*size3_F):print("size:",size1,",",size2)
+        if(i%100 == 0):print(i,"/",amount)
+        array = numpy.array(resized[i])
+        a = numpy.reshape(array,(size1*size2*size3))
+        reshaped = numpy.concatenate((reshaped,[a]),0)
+    return reshaped
+    
    
 print("loading data...")
 size = 100
@@ -42,23 +54,16 @@ print("grayscaling...")
 resized = util.loading_map(color.rgb2gray, resized)
 
 print("reshaping to array...")
-size1_F = len(resized[0])
-size2_F = len(resized[0][0])
-#♥print("size:",size1_F,",",size2_F)
-array = numpy.array(resized[0])
-reshaped = numpy.reshape(array,(size1_F*size2_F))
-reshaped = [reshaped]
-for i in range(1,amount):
-    size1 = len(resized[i])
-    size2 = len(resized[i][0])
-    if(size1*size2 != size1_F*size2_F):print("size:",size1,",",size2)
-    if(i%100 == 0):print(i,"/",amount)
-    array = numpy.array(resized[i])
-    a = numpy.reshape(array,(size1*size2))
-    reshaped = numpy.concatenate((reshaped,[a]),0)
+reshaped = flatten(resized)
     
 #♠print("dimensions reshaped:",len(reshaped),",",len(reshaped[0]),",",len(reshaped[0][0]))
     
+print("hsv...")
+hsv = util.loading_map(color.rgb2hsv, resized)
+hsv = flatten(hsv)
+print("luv...")
+luv = util.loading_map(color.rgb2luv, resized)  
+luv = flatten(luv)  
 
 #model = svm.SVC(kernel='linear', probability = True)
 from sklearn import random_projection
@@ -70,11 +75,15 @@ model = Pipeline([
     #("lda projection", lda.LDA(n_components = 80)),
     #("gaussian random projection", random_projection.GaussianRandomProjection(n_components = 150)),
     #("sparse random projection", random_projection.SparseRandomProjection(n_components = 350)),
-    ("Multi-layer Perceptron", MLPClassifier(algorithm='sgd', hidden_layer_sizes=(100,64,64,64,64,64,64,64,81), random_state=1,learning_rate='constant'))
+    ("Multi-layer Perceptron", MLPClassifier(algorithm='adam', hidden_layer_sizes=(100,64,64,64,64,64,64,64,81), random_state=1,learning_rate='constant'))
     #("svm", svm.SVC(kernel = "sigmoid", C = 1000, gamma = 0.0001, probability = True))
     ])
     
 n_folds = 5
-print("Evaluating featureless covnet")
+print("featureless")
 validation.validate_feature(reshaped, labels, classes, model, n_folds, False, False, True, True)
+print("hsv features")
+validation.validate_feature(hsv, labels, classes, model, n_folds, False, False, True, True)
+print("luv features")
+validation.validate_feature(luv, labels, classes, model, n_folds, False, False, True, True)
 print('\a')
